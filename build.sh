2dcd43010161276c5933d50817e39af05567d0a0
Marko-Vujnovic <errmalt@gmail.com>
1994-06-08 01:36:57 +0200

    JetbrainsIdea.sh ssh://git@github.com/___ url so that auth uses the key from $HOME/.ssh/ for private repos

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-# CARGO_NET_GIT_FETCH_WITH_CLI=true cargo build --release --color always 2>&1 | less -r
+
+# rm -rf $HOME/.cargo/.package-cache # Solves: "waiting for file lock on package cache"
 CARGO_NET_GIT_FETCH_WITH_CLI=true cargo build --release --color always 2>&1
 RUST_BACKTRACE=1 cargo test --release
--- tests/tests.rs
+++ tests/tests.rs
@@ -2,21 +2,21 @@
 
 // static programName: &'static str = "mpa";
 struct ProgramInfo { name: &'static str }
-static program_info: ProgramInfo = ProgramInfo {
+static PROGRAM_INFO: ProgramInfo = ProgramInfo {
     name: "mpa",
 };
 
-fn get_app_folder() -> std::path::PathBuf { let mut dir = std::path::PathBuf::new(); dir.push(&std::env::var("HOME").unwrap()); dir.join(&program_info.name) }
+fn get_app_folder() -> std::path::PathBuf { let mut dir = std::path::PathBuf::new(); dir.push(&std::env::var("HOME").unwrap()); dir.join(&PROGRAM_INFO.name) }
 fn cwd() -> std::path::PathBuf { std::env::current_dir().unwrap() }
 
 use marko_plaintext_archive::*;
 
-fn mpa_str(folder: &std::path::PathBuf) -> Result<String, std::io::Error> { Ok(serde_json::to_string(&pack(&folder)?)?) }
-fn folders_are_the_same(left: &std::path::PathBuf, right: &std::path::PathBuf) -> Result<bool, std::io::Error> { Ok(mpa_str(&left)?.eq(&mpa_str(&right)?)) }
+fn mpa_str(folder: &std::path::PathBuf) -> core::result::Result<String, std::io::Error> { Ok(serde_json::to_string(&pack(&folder)?)?) }
+fn folders_are_the_same(left: &std::path::PathBuf, right: &std::path::PathBuf) -> core::result::Result<bool, std::io::Error> { Ok(mpa_str(&left)?.eq(&mpa_str(&right)?)) }
 
 
 #[test]
-fn roundtripT() -> Result<(), std::io::Error> { tokio::runtime::Runtime::new().unwrap().block_on(async {
+fn roundtrip_t() -> core::result::Result<(), std::io::Error> { tokio::runtime::Runtime::new().unwrap().block_on(async {
     std::fs::create_dir_all(&get_app_folder())?;
 
     let orig_folder = cwd().join("tests/ExampleProject");
@@ -29,10 +29,10 @@ fn roundtripT() -> Result<(), std::io::Error> { tokio::runtime::Runtime::new().u
 
     assert!(folders_are_the_same(&orig_folder, &produced_folder)?);
 
-    let jsonFile = cwd().join(format!("tests/{}.mpa", "ExampleProject")); // .mpa - "Marko plain-text archive"
-    let scriptProject: marko_fs_types::Folder = serde_json::from_str(&std::fs::read_to_string(&jsonFile).expect("Something went wrong reading the file"))?;
+    let json_file = cwd().join(format!("tests/{}.mpa", "ExampleProject")); // .mpa - "Marko plain-text archive"
+    let script_project: marko_fs_types::Folder = serde_json::from_str(&std::fs::read_to_string(&json_file).expect("Something went wrong reading the file"))?;
 
-    writeln!(std::fs::File::create(cwd().join(format!("produced/{}.mpa", "ExampleProject")))?, "{}", serde_json::to_string(&scriptProject)?)?;
+    writeln!(std::fs::File::create(cwd().join(format!("produced/{}.mpa", "ExampleProject")))?, "{}", serde_json::to_string(&script_project)?)?;
 
 Ok(())})}
 
